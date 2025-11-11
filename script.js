0527d13b59a8bad7b792212de553209ec0ae56ce
JonGudenzi <jongudenzi@gmail.com>
2025-11-10 22:54:27 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Todo app loaded");
+
+const todoForm = document.getElementById("todoForm");
+const todoInput = document.getElementById("todoInput");
+const todoList = document.getElementById("todoList");
